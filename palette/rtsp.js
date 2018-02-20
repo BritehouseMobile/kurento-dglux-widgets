954a5a58e7678a5dcec51765e42aada6f365ed1d
Julian Villalva <j.villalva@dglogik.com>
2018-02-19 16:07:45 -0800

    Make video autoplay = true. clean up debug output. parameter changes and additions

--- palette/rtsp.js
+++ palette/rtsp.js
@@ -1,124 +1,13 @@
-var args = {
-        ws_uri: 'wss://10.0.1.128:283/kurento',
-        address: 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov',
-        ice_servers: undefined
-};
+var currentWidgets = [];
 
-var pipeline;
-var player;
-var webRtcPeer;
-var paused;
-
-function start() {
-    var videoOutput = document.getElementById('videoOutput');
-    
-    if(!args.address){
-        window.alert("You must set the video source URL first");
-        return;
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
     }
-    var options = {
-        remoteVideo : videoOutput
-    };
-    webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options,
-        function(error){
-            if(error){
-                return console.error(error);
-            }
-            webRtcPeer.generateOffer(onOffer);
-            webRtcPeer.peerConnection.addEventListener('iceconnectionstatechange', function(event){
-                if(webRtcPeer && webRtcPeer.peerConnection){
-                    console.log("oniceconnectionstatechange -> " + webRtcPeer.peerConnection.iceConnectionState);
-                    console.log('icegatheringstate -> ' + webRtcPeer.peerConnection.iceGatheringState);
-                }
-            });
-        });
-}
-function pause() {
-    if (!paused) {
-        document.getElementById("videoOutput").pause();
-        player.pause();
-        paused = true;
-    } else {
-        document.getElementById("videoOutput").play();
-        player.play();
-        paused = false;
-    }
-}
-
-function onOffer(error, sdpOffer){
-    if(error) return onError(error);
-
-    kurentoClient(args.ws_uri, function(error, kurentoClient) {
-        if(error) return onError(error);
-
-        kurentoClient.create("MediaPipeline", function(error, p) {
-            if(error) return onError(error);
-
-            pipeline = p;
-
-            pipeline.create("PlayerEndpoint", {uri: args.address}, function(error, pl){
-                if(error) return onError(error);
-                
-                player = pl;
-
-                pipeline.create("WebRtcEndpoint", function(error, webRtcEndpoint){
-                    if(error) return onError(error);
-
-                    setIceCandidateCallbacks(webRtcEndpoint, webRtcPeer, onError);
-
-                    webRtcEndpoint.processOffer(sdpOffer, function(error, sdpAnswer){
-                        if(error) return onError(error);
-
-                        webRtcEndpoint.gatherCandidates(onError);
-
-                        webRtcPeer.processAnswer(sdpAnswer);
-                    });
-
-                    player.connect(webRtcEndpoint, function(error){
-                        if(error) return onError(error);
-
-                        console.log("PlayerEndpoint-->WebRtcEndpoint connection established");
-
-                        player.play(function(error){
-                            if(error) return onError(error);
-
-                            document.getElementById("videoOutput").play();
-                            console.log("Player playing ...");
-                        });
-                    });
-                });
-            });
-        });
-    });
-}
-
-function stop() {
-    if (webRtcPeer) {
-        webRtcPeer.dispose();
-        webRtcPeer = null;
-    }
-    if(pipeline){
-        pipeline.release();
-        pipeline = null;
-    }
-}
-
-function setIceCandidateCallbacks(webRtcEndpoint, webRtcPeer, onError){
-    webRtcPeer.on('icecandidate', function(candidate){
-        console.log("Local icecandidate " + JSON.stringify(candidate));
-
-        candidate = kurentoClient.register.complexTypes.IceCandidate(candidate);
-
-        webRtcEndpoint.addIceCandidate(candidate, onError);
-
-    });
-    webRtcEndpoint.on('OnIceCandidate', function(event){
-        var candidate = event.candidate;
-
-        console.log("Remote icecandidate " + JSON.stringify(candidate));
-
-        webRtcPeer.addIceCandidate(candidate, onError);
-    });
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
 }
 
 function onError(error) {
@@ -140,18 +29,139 @@ define([
         function MyWidget(div, model) {
             _super.call(this, div, model);
 
+            this.videoName = guid();
+
             this.videoElement = document.createElement('video');
-            this.videoElement.id = "videoOutput";
+            this.videoElement.id = this.videoName;
             this.videoElement.style.position = "absolute";
             this.videoElement.style.width = "100%";
+            this.videoElement.autoplay = true;
+
+            this.paused = false;
             div.appendChild(this.videoElement);
-            
+
+            currentWidgets.push(this);
         }
-        
+
         MyWidget.prototype.timer = 0;
-        
+
         MyWidget.prototype.seekTime = 0;
-        
+
+        MyWidget.prototype.args = {
+            ws_uri: 'wss://10.0.1.128:283/kurento',
+            address: 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov',
+            ice_servers: undefined
+        };
+
+        MyWidget.prototype.setIceCandidateCallbacks = function (webRtcEndpoint, webRtcPeer, onError){
+            webRtcPeer.on('icecandidate', function(candidate){
+                candidate = kurentoClient.register.complexTypes.IceCandidate(candidate);
+                webRtcEndpoint.addIceCandidate(candidate, onError);
+            });
+            webRtcEndpoint.on('OnIceCandidate', function(event){
+                var candidate = event.candidate;
+                webRtcPeer.addIceCandidate(candidate, onError);
+            });
+        };
+
+        MyWidget.prototype.pause = function () {
+            if (!this.paused) {
+                document.getElementById(this.videoName).pause();
+                this.player.pause();
+                this.paused = true;
+            } else {
+                document.getElementById(this.videoName).play();
+                this.player.play();
+                this.paused = false;
+            }
+        }
+
+        MyWidget.prototype.stop = function () {
+            clearInterval(this.timer);
+
+            if (this.webRtcPeer) {
+                this.webRtcPeer.dispose();
+                this.webRtcPeer = null;
+            }
+            if(this.pipeline){
+                this.pipeline.release();
+                this.pipeline = null;
+            }
+        }
+
+
+        MyWidget.prototype.start = function () {
+            var widget = this;
+            var videoOutput = document.getElementById(this.videoName);
+
+            if(!this.args.address){
+                return;
+            }
+
+            var options = {
+                remoteVideo : videoOutput
+            };
+
+            widget.webRtcPeer = kurentoUtils.WebRtcPeer.WebRtcPeerRecvonly(options,
+                function(error){
+                    if(error){
+                        return console.error(error);
+                    }
+                    widget.webRtcPeer.generateOffer(function(error, sdpOffer) {
+                        if(error) return onError(error);
+
+                        var kc = kurentoClient(widget.args.ws_uri, function(error, kurentoClient) {
+                            if(error) return onError(error);
+
+                            kurentoClient.create("MediaPipeline", function(error, p) {
+                                if(error) return onError(error);
+
+                                widget.pipeline = p;
+
+                                widget.pipeline.create("PlayerEndpoint", {uri: widget.args.address}, function(error, pl){
+                                    if(error) return onError(error);
+
+                                    widget.player = pl;
+
+                                    widget.pipeline.create("WebRtcEndpoint", function(error, webRtcEndpoint){
+                                        if(error) return onError(error);
+
+                                        widget.setIceCandidateCallbacks(webRtcEndpoint, widget.webRtcPeer, onError);
+
+                                        webRtcEndpoint.processOffer(sdpOffer, function(error, sdpAnswer){
+                                            if(error) return onError(error);
+
+                                            webRtcEndpoint.gatherCandidates(onError);
+
+                                            widget.webRtcPeer.processAnswer(sdpAnswer);
+                                        });
+
+                                        widget.player.connect(webRtcEndpoint, function(error){
+                                            if(error) return onError(error);
+
+                                            console.log("PlayerEndpoint-->WebRtcEndpoint connection established");
+
+                                            widget.player.play(function(error){
+                                                if(error) return onError(error);
+
+                                                widget.updateModelValue("sessionId", kc.sessionId);
+                                                widget.updateModelValue("object", widget.player.id.split("/")[0]);
+                                            });
+                                        });
+                                    });
+                                });
+                            });
+                        });
+                    });
+                    widget.webRtcPeer.peerConnection.addEventListener('iceconnectionstatechange', function(event){
+                        if(widget.webRtcPeer && widget.webRtcPeer.peerConnection){
+                            console.log("oniceconnectionstatechange -> " + widget.webRtcPeer.peerConnection.iceConnectionState);
+                            console.log('icegatheringstate -> ' + widget.webRtcPeer.peerConnection.iceGatheringState);
+                        }
+                    });
+                });
+        }
+
         MyWidget.prototype.getDefinition = function () {
             return {
                 "name": "",
@@ -163,61 +173,103 @@ define([
                     { "t": "string", "n": "sourceUri" },
                     { "t": "number", "n": "currentTime" },
                     { "t": "number", "n": "seekTime" },
-                    { "t": "trigger", "n": "seek" }
+                    { "t": "string", "n": "videoWidth" },
+                    { "t": "string", "n": "videoHeight" },
+                    { "t": "bool", "n": "isMediaFlowingOut" },
+                    { "t": "trigger", "n": "seek" },
+                    { "t": "bool", "n": "mute" },
+                    { "t": "string", "n": "sessionId" },
+                    { "t": "string", "n": "object" }
                 ],
                 "layout": {
                     "type": "vbox",
-                    "children": ["start", "pause", "stop", "wsUri", "sourceUri", "currentTime", "seekTime", "seek"] // order of properties
+                    "children": ["start", "pause", "stop", "wsUri", "sourceUri", "currentTime", "seekTime", "videoWidth", "videoHeight", "isMediaFlowingOut", "seek", "mute", "sessionId", "object"] // order of properties
                 }
             };
         };
-        
+
         MyWidget.myPropMap = {
             "start": function (widget, value) {
                 if (value != undefined) {
-                    start();
-                    
-                    this.timer = setInterval(function() {
-                        if (player != undefined) {
-                            player.getPosition().then(function(result) {
-                                console.log("setting current time to " + result);
+                    if (widget.paused == true) {
+                        widget.pause();
+                        return;
+                    }
+                    widget.stop();
+                    widget.start();
+                    widget.timer = setInterval(function() {
+                        if (widget.player != undefined) {
+                            widget.player.getPosition().then(function(result) {
+                                console.log("Current time: " + result);
                                 widget.updateModelValue('currentTime', result);
+                                widget.updateModelValue('videoWidth', widget.videoElement.videoWidth);
+                                widget.updateModelValue('videoHeight', widget.videoElement.videoHeight);
+                                widget.player.isMediaFlowingOut("VIDEO").then(function(result) {
+                                    widget.updateModelValue("isMediaFlowingOut", result);
+                                });
                             });
                         }
                     }, 1000);
+                    
+                    // Finally play the video after real media playback has begun
+                    widget.videoElement.play();
                 }
             },
             "pause": function(widget, value) {
                 if (value != undefined) {
-                    player.getPosition().then(function(result) {
+                    widget.player.getPosition().then(function(result) {
                         widget.updateModelValue('currentTime', result);
                     });
-                    pause();
+                    widget.pause();
                 }
             },
             "stop": function (widget, value) {
                 if (value != undefined) {
-                    stop();
-                    clearInterval(this.timer);
+                    widget.stop();
+                    widget.updateModelValue("isMediaFlowingOut", false);
                 }
             },
             "wsUri": function (widget, value) {
                 if (value != undefined)
-                    args.ws_uri = value;
+                    widget.args.ws_uri = value;
             },
             "sourceUri": function (widget, value) {
                 if (value != undefined)
-                    args.address = value;
+                    widget.args.address = value;
+                else
+                    widget.args.address = null;
+            },
+            "currentTime": function (widget, value) {
             },
             "seekTime": function (widget, value) {
                 if (value != undefined)
-                    this.seekTime = value;
+                    widget.seekTime = value;
+            },
+            "videoWidth": function(widget, value) {
+            },
+            "videoHeight": function(widget, value) {
+            },
+            "isMediaFlowingOut": function(widget, value) {
             },
             "seek": function (widget, value) {
                 if (value != undefined) {
-                    player.setPosition(this.seekTime).then(function() {
+                    console.log("Seeking to: " + widget.seekTime);
+                    widget.player.setPosition(widget.seekTime).then(function(result) {
                     });
                 }
+            },
+            "mute": function (widget, value) {
+                if (value != undefined) {
+                    if (value) {
+                        document.getElementById(widget.videoName).muted = true;
+                    } else {
+                        document.getElementById(widget.videoName).muted = false;
+                    }
+                }
+            },
+            "sessionId": function (widget, value) {
+            },
+            "object": function (widget, value) {
             }
         };
 
@@ -226,9 +278,12 @@ define([
         };
 
         MyWidget.prototype.destroy = function () {
-            this.videoOutput.remove();
+            this.videoElement.remove();
+            var index = currentWidgets.indexOf(this);
+            if (index > -1) currentWidgets.splice(index, 1);
+            this.stop();
         };
-        
+
         return MyWidget;
     }(dgluxjs.Widget));
 
